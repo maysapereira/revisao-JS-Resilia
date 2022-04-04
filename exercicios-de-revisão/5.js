8a5bb0c14c5e817f78fef68bf558bdd4d7ee0c00
Maysa Pereira <93635431+maysapereira@users.noreply.github.com>
2022-04-04 04:59:51 -0300

    :sparkles: adiciona exercio 5

---
+++ exercicios-de-revisão/5.js
@@ -0,0 +1,15 @@
+/*
+
+Rest & Spread
+
+Utilizando um operador clone o objeto clothes
+
+const clothes = {
+  pants: { colors: ['black', 'blue'] },
+  shirts: { colors: ['white', 'red'] },
+  socks: { colors: ['beige', 'gray'] },
+};
+
+*/
+
+const roupas = {...clothes}
