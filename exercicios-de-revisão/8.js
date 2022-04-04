c752a46a048ae00c154e59c4ae0853cd27156049
Maysa Pereira <93635431+maysapereira@users.noreply.github.com>
2022-04-04 05:12:49 -0300

    :sparkles: adiciona exercicio 8

---
+++ exercicios-de-revisão/8.js
@@ -0,0 +1,10 @@
+/*
+
+A partir da variável abaixo e utilizando um método de array, 
+multiplique todos os valores do array por 10. Utilize arrow function.
+
+var numbers = [65, 44, 12, 4];
+
+*/
+
+const multiplica = numbers.map((number) => number * 10)
