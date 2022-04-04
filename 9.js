25927631248ff226624df63b4c5f71589b8fed9a
Maysa Pereira <93635431+maysapereira@users.noreply.github.com>
2022-04-04 05:14:54 -0300

    :sparkles: adiciona exercicio 9

---
+++ 9.js
@@ -0,0 +1,10 @@
+/*
+
+A partir da variável abaixo retorne um array com valores de 
+age (idade) maiores ou iguais a 18 com arrow function e filter
+
+var ages = [32, 33, 16, 40];
+
+*/
+
+const filtraIdades = ages.filter((age) => age >= 18);
