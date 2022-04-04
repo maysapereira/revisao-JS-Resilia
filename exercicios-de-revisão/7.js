9beb47faf43ecb515f8f85c9ac9cb36c5cafd0c9
Maysa Pereira <93635431+maysapereira@users.noreply.github.com>
2022-04-04 05:09:42 -0300

    :sparkles: adiciona exercio 7

---
+++ exercicios-de-revisão/7.js
@@ -0,0 +1,12 @@
+/*
+
+Arrays e seus métodos
+
+A partir da variável abaixo e utilizando um método de array, 
+retorne a raiz quadrada de todos os valores do array original em um novo array
+
+var numbers = [4, 9, 16, 25];
+
+*/
+
+const raizQuadrada = numbers.map(number=> Math.sqrt(number))
