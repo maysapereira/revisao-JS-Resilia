3e4d602f998f60b1035057f1b5b5e75b87866e39
Maysa Pereira <93635431+maysapereira@users.noreply.github.com>
2022-04-04 05:19:45 -0300

    :sparkles: adiciona exercicio 10

---
+++ exercicios-de-revisão/10.js
@@ -0,0 +1,31 @@
+/*
+
+A partir do objeto abaixo, retorne somente os nomes dos objetos 
+do tipo cachorro (​type: dog)​ com filter
+data = [
+    {
+      name: 'Butters',
+      age: 3,
+      type: 'dog',
+    },
+    {
+      name: 'Lizzy',
+      age: 6,
+      type: 'dog',
+    },
+  
+    {
+      name: 'Red',
+      age: 1,
+      type: 'cat',
+    },
+    {
+      name: 'Joey',
+      age: 3,
+      type: 'dog',
+    },
+  ];
+
+  */
+
+  const dogs = data.filter((animal) => animal.type === 'dog');
