c242e32e5e97845b881b4226c559b7f0acdb8d3e
Maysa Pereira <93635431+maysapereira@users.noreply.github.com>
2022-04-04 04:52:22 -0300

    :sparkles: adiciona exercicio 3

---
+++ exercicios-de-revisão/3.js
@@ -0,0 +1,12 @@
+/*
+
+A partir do objeto abaixo, instancie apenas uma variável
+que receba first utilizando destructuring
+
+const obj = { first: 'Jane', last: 'Doe' };
+
+*/
+
+const { first } = obj
+
+console.log(first)
